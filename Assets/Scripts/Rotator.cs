57cdec86039bc0a029d4189a61d81001efd7341d
Lucas Reyna <lucas.reyna143@gmail.com>
2021-02-09 09:17:00 -0800

    Added Virus Level

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float xValue;
+    public float yValue;
+    public float zValue;
     
     // Before rendering each frame..
     void Update()
@@ -11,6 +14,6 @@ public class Rotator : MonoBehaviour
         // Rotate the game object that this script is attached to by 15 in the X axis,
         // 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
         // rather than per frame.
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(xValue, yValue, zValue) * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/VirusKillObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VirusKillObject : MonoBehaviour
+{
+    public TextMeshProUGUI lostText;
+    public GameObject winTextObject;
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lostText.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Sanitizer"))
+        {
+            this.gameObject.SetActive(false);
+            audioSource.Play();
+        }
+    }
+}
